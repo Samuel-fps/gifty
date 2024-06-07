6c78ce5f98680b12e91faba95a39b8f76e63296f
Samuel <86070772+Samuel-fps@users.noreply.github.com>
2024-06-07 12:16:16 +0200

    feature-User save with encoded password and add register user to user service

--- src/main/java/com/gifty/application/user/UserService.java
+++ src/main/java/com/gifty/application/user/UserService.java
@@ -1,15 +1,47 @@
 package com.gifty.application.user;
 
 
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.server.VaadinSession;
+import com.vaadin.flow.server.WrappedSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
 
-    public UserService(UserRepository repository) {
+    private PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository repository, PasswordEncoder passwordEncoder) {
         this.userRepository = repository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public void registerUser(User u) {
+        String encodedPassword = passwordEncoder.encode(u.getPassword());
+        User user = new User(u.getName(), u.getLastname(), u.getEmail(), encodedPassword);
+        userRepository.save(user);
+    }
+
+    public boolean validateCredentials(String email, String password){
+        User user = userRepository.findByEmail(email);
+        Notification.show("Entrando", 3000, Notification.Position.MIDDLE);
+
+        if (user != null && passwordEncoder.matches(password, user.getPassword())){
+            // Autenticación exitosa, guardar información de usuario en la sesión
+            VaadinSession session = VaadinSession.getCurrent();
+            WrappedSession wrappedSession = session.getSession();
+
+            // Guardar información relevante del usuario en la sesión
+            wrappedSession.setAttribute("user", user);
+            return true;
+        }
+
+    return false;
     }
 
 }
