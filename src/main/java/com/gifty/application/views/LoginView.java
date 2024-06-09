43b45dc3f4a76e54304263c1ec208789f8386d22
Samuel <86070772+Samuel-fps@users.noreply.github.com>
2024-06-09 22:32:58 +0200

    feature-Lenguage configuration

--- src/main/java/com/gifty/application/views/LoginView.java
+++ src/main/java/com/gifty/application/views/LoginView.java
@@ -1,17 +1,11 @@
 package com.gifty.application.views;
 
-import com.gifty.application.data.user.UserService;
 import com.gifty.application.security.AuthenticatedUser;
 import com.vaadin.flow.component.UI;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
-import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.login.AbstractLogin;
 import com.vaadin.flow.component.login.LoginForm;
 import com.vaadin.flow.component.login.LoginI18n;
 import com.vaadin.flow.component.login.LoginOverlay;
-import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.PageTitle;
@@ -19,12 +13,13 @@ import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.internal.RouteUtil;
 import com.vaadin.flow.server.VaadinService;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
-import com.vaadin.flow.spring.security.AuthenticationContext;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.context.MessageSource;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Locale;
 
 @Route("login")
 @PageTitle("Login")
@@ -34,78 +29,39 @@ public class LoginView extends LoginOverlay implements BeforeEnterObserver {
     private final LoginForm login = new LoginForm();
 
     private final AuthenticatedUser authenticatedUser;
+    private final MessageSource messageSource;
 
     @Autowired
-    public LoginView(AuthenticatedUser authenticatedUser) {
+    public LoginView(AuthenticatedUser authenticatedUser, MessageSource messageSource) {
         this.authenticatedUser = authenticatedUser;
+        this.messageSource = messageSource;
+
         setAction(RouteUtil.getRoutePath(VaadinService.getCurrent().getContext(), getClass()));
-
-        LoginI18n i18n = LoginI18n.createDefault();
-        i18n.setHeader(new LoginI18n.Header());
-        i18n.getHeader().setTitle("Gifty");
-        i18n.getHeader().getDescription();
-        i18n.setAdditionalInformation(null);
-        setI18n(i18n);
-
+        configureLoginI18n();
         setForgotPasswordButtonVisible(false);
         setOpened(true);
     }
-    /*
-    @Autowired
-    public LoginView(AuthenticationContext authenticationContext, UserService userService) {
-        this.userService = userService;
 
-        // style
-        addClassName("login-view");
-        setSizeFull();
-        setAlignItems(Alignment.CENTER);
-        setJustifyContentMode(JustifyContentMode.CENTER);
-
-        // Login form
-        LoginForm loginForm = new LoginForm();
-        loginForm.addLoginListener(this::handleLogin);
-
-        // Register view
-        Button signinButton = new Button("Sign in");
-        signinButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
-        signinButton.addClickListener(e -> getUI().ifPresent(ui -> ui.navigate(RegistrationView.class)));
-
-        add(new H1("Gifty"), loginForm, signinButton);
+    private void configureLoginI18n() {
+        Locale locale = VaadinService.getCurrentRequest().getLocale();
+        LoginI18n i18n = LoginI18n.createDefault();
+        i18n.setHeader(new LoginI18n.Header());
+        i18n.getHeader().setTitle(messageSource.getMessage("login.title", null, locale));
+        i18n.getHeader().setDescription(messageSource.getMessage("login.description", null, locale));
+        i18n.setAdditionalInformation(messageSource.getMessage("login.additionalInformation", null, locale));
+        i18n.getForm().setTitle(messageSource.getMessage("login.formTitle", null, locale));
+        i18n.getForm().setUsername(messageSource.getMessage("login.formEmail", null, locale));
+        i18n.getForm().setPassword(messageSource.getMessage("login.formPassword", null, locale));
+        i18n.getForm().setSubmit(messageSource.getMessage("login.formSubmit", null, locale));
+        setI18n(i18n);
     }
-    */
-
-     /*
-    // Validate credentials
-    private void handleLogin(AbstractLogin.LoginEvent event) {
-        String email = event.getUsername();
-        String password = event.getPassword();
-
-        if (userService.validateCredentials(email, password)) {
-            UserDetails userDetails = userService.loadUserByUsername(email);
-            Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-            getUI().ifPresent(ui -> ui.navigate(""));
-        } else {
-            Notification.show("Invalid email or password.", 3000, Notification.Position.MIDDLE);
-            event.getSource().setError(true);
-        }
-    }
-    */
-
 
     @Override
     public void beforeEnter(BeforeEnterEvent beforeEnterEvent) {
-        // verify if the user is log in
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null && authentication.isAuthenticated()) {
-            UI.getCurrent().navigate(MainView.class);
+        if (authenticatedUser.get().isPresent()) {
+            setOpened(false);
+            beforeEnterEvent.forwardTo("");
         }
-        // inform the user about an authentication error
-        if(beforeEnterEvent.getLocation()
-                .getQueryParameters()
-                .getParameters()
-                .containsKey("error")) {
-            login.setError(true);
-        }
+        setError(beforeEnterEvent.getLocation().getQueryParameters().getParameters().containsKey("error"));
     }
 }
