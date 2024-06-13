e8d677a6d6af34e77e65571245cdf7e4ae18aab6
Samuel <86070772+Samuel-fps@users.noreply.github.com>
2024-06-14 01:28:37 +0200

    feature-login button

--- src/main/java/com/gifty/application/views/login/RegistrationView.java
+++ src/main/java/com/gifty/application/views/login/RegistrationView.java
@@ -2,9 +2,11 @@ package com.gifty.application.views.login;
 
 import com.gifty.application.login.RegistrationForm;
 import com.gifty.application.login.RegistrationFormBinder;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouterLink;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -25,5 +27,13 @@ public class RegistrationView extends VerticalLayout {
         add(registrationForm);
 
         registrationFormBinder.addBindingAndValidation();
+
+        // Añadir el enlace de inicio de sesión
+        Span loginPrompt = new Span("¿Ya estás registrado? ");
+        RouterLink loginLink = new RouterLink("inicia sesión", LoginView.class);
+        VerticalLayout loginLayout = new VerticalLayout(loginPrompt, loginLink);
+        loginLayout.setAlignItems(Alignment.CENTER);
+
+        add(loginLayout);
     }
 }
